5b696a586eaa39529a235ccd5dcb9df63a79ecb8
Boris <borisculjak@gmail.com>
2024-05-20 22:54:46 +0200

    Added icons

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -75,7 +75,7 @@ namespace Winery
             dataGridContainers.ItemsSource = WineryContext.Instance.Containers.Local;
         }
 
-        private void EditContainerButton_Click(object sender, RoutedEventArgs e)
+        private void EditContainer_Click(object sender, RoutedEventArgs e)
         {
             if (SelectedContainer != null)
             {
@@ -87,6 +87,25 @@ namespace Winery
             else
             {
                 MessageBox.Show("Please select a container to edit.", "No Selection", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void DeleteContainer_Click(object sender, RoutedEventArgs e)
+        {
+            if (SelectedContainer != null)
+            {
+                MessageBoxResult result = MessageBox.Show("Are you sure you want to delete this container?", "Delete Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    WineryContext.Instance.Containers.Remove(SelectedContainer);
+                    WineryContext.Instance.SaveChanges();
+                    dataGridContainers.ItemsSource = null;
+                    dataGridContainers.ItemsSource = WineryContext.Instance.Containers.Local;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a container to delete.", "No Selection", MessageBoxButton.OK, MessageBoxImage.Information);
             }
         }
 
